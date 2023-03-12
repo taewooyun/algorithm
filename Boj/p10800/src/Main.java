c50bc19b6d21dc5c42bb57fc40b689fe5c7363d1
taewooyun <taewooyunn@gmail.com>
2023-03-12 22:51:26 +0900

    boj: 10800

--- Boj/p10800/src/Main.java
+++ Boj/p10800/src/Main.java
@@ -1,5 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static class Player implements Comparable<Player>{
+        int idx, color, size;
+
+        public Player(int idx, int color, int size) {
+            this.idx = idx;
+            this.color = color;
+            this.size = size;
+        }
+
+        @Override
+        public int compareTo(Player o) {
+            return this.size - o.size;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Player[] players = new Player[n];
+
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int color = Integer.parseInt(st.nextToken());
+            int size = Integer.parseInt(st.nextToken());
+
+            players[i] = new Player(i, color, size);
+        }
+
+        Arrays.sort(players);
+
+        int[] colors = new int[n];
+        int[] scores = new int[n];
+        int idx = 0;
+        int total = 0;
+
+        for(int i=0; i<n; i++){
+            Player current = players[i];
+
+            while(players[idx].size < current.size) {
+                total += players[idx].size;
+                colors[players[idx].color-1] += players[idx].size;
+                idx++;
+            }
+            scores[current.idx] = total - colors[current.color-1];
+        }
+
+        for(int i : scores){
+            System.out.println(i);
+        }
     }
 }
