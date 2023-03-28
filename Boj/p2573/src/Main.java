eadd336b3dde51ef4fb1ace975b45266016c0718
taewooyun <taewooyunn@gmail.com>
2023-03-28 23:51:37 +0900

    boj: 2573

---
+++ Boj/p2573/src/Main.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int r;
+    static int c;
+    static int[][] map;
+    static int[][] visited;
+    static int[][] melt;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        map = new int[r][c];
+        visited = new int[r][c];
+        melt = new int[r][c];
+
+        for(int i=0; i<r; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<c; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        solution();
+    }
+
+    static void solution() {
+        int year = 0;
+
+        while(true) {
+            int count = 0;
+            for(int i=0; i<r; i++) {
+                for(int j=0; j<c; j++) {
+                    if(visited[i][j] == 0 && map[i][j] != 0) {
+                        dfs(i, j);
+                        count++;
+                    }
+                }
+            }
+
+            if(count == 0) {
+                System.out.println(0);
+                break;
+            } else if(count >= 2) {
+                System.out.println(year);
+                break;
+            }
+
+            melting();
+            year++;
+        }
+    }
+
+    static void dfs(int x, int y) {
+        visited[x][y] = 1;
+
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(0 <= nx && nx < r && 0 <= ny && ny < c) {
+                // 1년 후에 녹는 빙산의 양 구하기
+                if(map[nx][ny] == 0)
+                    melt[x][y]++;
+
+                // dfs 재귀
+                if(visited[nx][ny] == 0 && map[nx][ny] != 0)
+                    dfs(nx, ny);
+            }
+        }
+    }
+
+    static void melting() {
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                map[i][j] -= melt[i][j];
+
+                if(map[i][j] < 0)
+                    map[i][j] = 0;
+
+                visited[i][j] = 0;
+                melt[i][j] = 0;
+            }
+        }
+    }
+}
