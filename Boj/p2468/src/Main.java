dc658006392f06471d4b41e34a409f42474a8cf3
taewooyun <taewooyunn@gmail.com>
2023-05-03 17:42:26 +0900

    boj: 2468
    add bfs

--- Boj/p2468/src/Main.java
+++ Boj/p2468/src/Main.java
@@ -2,56 +2,99 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
+    static int N;
     static int[][] map;
-    static boolean[][] checked;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0,- 1, 0, 1};
-    public static void main(String[] args) throws IOException{
+    static boolean[][] visit;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int cx, cy;
+    static int amount;
+
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        map = new int[n][n];
+        map = new int[N][N];
 
-        int maxHeight=0;
-        for(int i=0; i<n; i++) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] > maxHeight) {
-                    maxHeight =map[i][j];
-                }
+
+            for(int j=0; j<N; j++){
+                int height = Integer.parseInt(st.nextToken());
+                pq.offer(height);
+
+                map[i][j] = height;
             }
         }
 
-        int max =0;
-        for(int height=0; height<maxHeight+1; height++) {
-            checked = new boolean[n][n];
-            int cnt=0;
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<n; j++) {
-                    if(!checked[i][j] && map[i][j] > height){
-                        cnt+=dfs(i,j,height);
+        int maxAmount = pq.peek();
+        pq.clear();
+        pq.offer(1);
+
+        for(int i=1; i<maxAmount; i++){
+            visit = new boolean[N][N];
+            int safeArea = 0;
+            amount = i;
+
+            for(int j=0; j<N; j++){
+                for(int k=0; k<N; k++){
+                    if(map[j][k] > amount && !visit[j][k]){
+                        safeArea++;
+                        dfs(k, j);
                     }
-
                 }
             }
-            max = Math.max(max, cnt);
-        }
-        System.out.println(max);
-    }
-    static int dfs(int x, int y, int height) {
-        checked[x][y] = true;
-        for(int i=0; i<4; i++) {
-            int nx = x +dx[i];
-            int ny = y +dy[i];
 
-            if(nx<0 || ny<0 || nx>n-1 || ny >n-1) continue;
-            if(checked[nx][ny]) continue;
-            if(map[nx][ny]> height) {
-                dfs(nx,ny, height);
+            pq.offer(safeArea);
+        }
+
+        System.out.println(pq.peek());
+    }
+
+    static void dfs(int x, int y){
+        visit[y][x] = true;
+
+        for(int i=0; i<4; i++){
+            cx = x + dx[i];
+            cy = y + dy[i];
+
+            if(inRange()){
+                if(!visit[cy][cx] && map[cy][cx] > amount){
+                    visit[cy][cx] = true;
+                    dfs(cx, cy);
+                }
             }
         }
-        return 1;
+    }
+
+    static void bfs(int x, int y){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {x, y});
+
+        visit[y][x] = true;
+
+        while (!q.isEmpty()) {
+            x = q.peek()[0];
+            y = q.poll()[1];
+
+            for(int i=0; i<4; i++){
+                cx = x + dx[i];
+                cy = y + dy[i];
+
+                if(inRange()){
+                    if(!visit[cy][cx] && map[cy][cx] > amount){
+                        q.offer(new int[] {cx, cy});
+
+                        visit[cy][cx] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    static boolean inRange(){
+        return 0 <= cx && cx < N && 0 <= cy && cy < N;
     }
 }
