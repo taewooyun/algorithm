3570bd07ad82157de2cd1fe8ed9e276853d2da6c
taewooyun <taewooyunn@gmail.com>
2023-05-03 18:13:53 +0900

    boj: 2606
    add bfs

--- Boj/p2606/src/Main.java
+++ Boj/p2606/src/Main.java
@@ -1,44 +1,62 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] a;
-    static boolean visit[];	// 정점 탐색여부 체크
-    static int n, m, v;		// 정점, 간선, 시작 정점
-    static int count = 0;	// 정점과 연결된 바이러스 걸리는 컴퓨터 수
+    static int computer, network;
 
-    public static int dfs(int i) {
-        visit[i] = true;
-        for(int k : a[i]) {
-            if(visit[k] == false) {
-                count ++;
-                dfs(k);
-            }
+    static int[][] map;
+    static boolean[] visit;
+    static int cnt;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        computer = Integer.parseInt(br.readLine());
+        network = Integer.parseInt(br.readLine());
+
+        map = new int[computer+1][computer+1];
+        visit = new boolean[computer+1];
+
+        for(int i=0; i<network; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            map[a][b] = map[b][a] = 1;
         }
-        return count;
+
+        dfs(1);
+
+        System.out.println(cnt);
     }
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        n = scan.nextInt();	// 컴퓨터 수(정점)
-        m = scan.nextInt();	// 연결된 컴퓨터 쌍의 수(간선)
-        v = 1;	// 탐색 시장할 정점의 번호
-        a = new ArrayList[n+1];	// 인덱스 편의상 n+1설정, 0번째 요소는 사용 X
-        visit = new boolean[n+1];
-        for(int i=1; i<=n; i++) {
-            a[i] = new ArrayList<Integer>();
+    static void dfs(int start){
+        visit[start] = true;
+
+        for(int i=1; i<=computer; i++){
+            if(!visit[i] && map[start][i] == 1){
+                cnt++;
+                dfs(i);
+            }
         }
+    }
 
-        for(int i=0; i<m; i++) {
-            int u = scan.nextInt();	// 간선으로 이어진 정점1
-            int v = scan.nextInt();	// 정점1과 간선으로 이어진 정점2
-            a[u].add(v);
-            a[v].add(u);
+    static void bfs(int start){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+
+        visit[start] = true;
+
+        while (!q.isEmpty()) {
+            start = q.poll();
+
+            for(int i=1; i<=computer; i++){
+                if(!visit[i] && map[start][i] == 1){
+                    cnt++;
+                    q.offer(i);
+                    visit[i] = true;
+                }
+            }
         }
-
-        System.out.println(dfs(v));
-
-        scan.close();
     }
 }
