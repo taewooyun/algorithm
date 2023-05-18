eb59de99856b510e804eba4b09c7519977f18af2
taewooyun <taewooyunn@gmail.com>
2023-05-18 15:32:35 +0900

    boj: 1699

---
+++ Boj/p1699/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+1];
+
+        for(int i=1; i<=n; i++){
+            dp[i] = i;
+            for(int j=2; j * j <= i; j++){
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
